50ed3e8d85c6999ad9ee1595ece27e35d479a198
Michał Sypniewski <mi.sypniewski@gmail.com>
2020-08-18 15:34:06 +0200

    feat: add animation in GSAP

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,41 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
-/* place your code below */
+console.log("HELLO 🚀");
 
-console.log('HELLO 🚀')
+const width = window.innerWidth;
+const animationSections = document.querySelectorAll(".animation--js");
+const paymentHTML = document.querySelector(".payment");
+
+if (width >= 768) {
+  animationSections.forEach((section) => {
+    gsap.fromTo(
+      section.children,
+      { y: "+=100", opacity: 0 },
+      {
+        y: 0,
+        opacity: 1,
+        stagger: 0.3,
+        duration: 0.8,
+        ease: "easeInOut",
+        scrollTrigger: { trigger: section, start: "top 40%" },
+      }
+    );
+  });
+
+  gsap.fromTo(
+    paymentHTML.children,
+    { y: "+=100", opacity: 0 },
+    {
+      y: 0,
+      opacity: 1,
+      stagger: 0.3,
+      duration: 0.8,
+      ease: "easeInOut",
+      scrollTrigger: { trigger: ".payment", start: "top 80%" },
+    }
+  );
+}
