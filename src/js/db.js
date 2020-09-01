395a3957be87af12f6edca074733f1619f4fd08b
Michał Sypniewski <mi.sypniewski@gmail.com>
2020-09-01 14:08:38 +0200

    feat: add fake db file

---
+++ src/js/db.js
@@ -0,0 +1,40 @@
+export const courses = [
+  {
+    date: "23.01.2020",
+    time: "16:00",
+  },
+  {
+    date: "24.02.2020",
+    time: "18:00",
+  },
+];
+
+export const products = [
+  {
+    title: "Kategoria B",
+    price: "1700 zł",
+    descryption: [
+      "30 godzin wykładów",
+      "30 godzin jazdy samochodem",
+      "Zestaw pytań testowych w formie pisemnej i multimedialnej",
+      "Egzamin wewnętrzny",
+      "Samochód Toyta Yaris",
+      "Materiały dydaktyczne",
+    ],
+  },
+  {
+    title: "Kurs uzupełniający z teorii",
+    price: "300 zł",
+    descryption: ["10 godzin", "Materiały dydaktyczne"],
+  },
+  {
+    title: "Kurs uzupełniający z jazdy praktycznej",
+    price: "400 zł",
+    descryption: ["5 godzin", "Samochód Toyta Yaris"],
+  },
+  {
+    title: "Kurs doszkalający",
+    price: "80 zł",
+    descryption: ["1 godzina", "Samochód Toyta Yaris"],
+  },
+];
